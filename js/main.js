9184d23650ab83ee87e11cb8303f77d4f873cc67
«KostiantynSamusiev» <djfoozyhook@gmail.com>
2022-07-04 08:17:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let explenationContainer = document.querySelectorAll('.game');
+let questionCOntainer = document.querySelectorAll('.top');
+let arrow = document.querySelectorAll('.arrow');
+
+
+let burgerMenu = document.querySelector('.burger');
+let navLinks = document.querySelector('.navigation-links');
+console.log(navLinks)
+
+
+
+burgerMenu.onclick = () => {
+  navLinks.classList.toggle('is-active');
+};
+
+  
+ //  Add css class to oped dropdown
+for (let i=0; i<questionCOntainer.length; i++){
+  questionCOntainer[i].onclick = function () {
+    explenationContainer[i].classList.toggle('is-active');
+    arrow[i].classList.toggle('rotate');
+  };
+}
+
+
+//  Carousel slider
+$('.games_card_container').slick({
+  infinite: true,
+  centerMode: false,
+  variableWidth: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+
+
+
+
+
+
+
+
+
+  
